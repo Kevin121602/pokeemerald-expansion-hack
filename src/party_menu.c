1c352b1caa3bbd65e98b53ad3bcbf9afd3f6f3eb
Kevin121602 <kevin.james.gracey@gmail.com>
2024-11-23 10:02:07 -0500

    adjusted level caps

--- src/party_menu.c
+++ src/party_menu.c
@@ -5575,7 +5575,7 @@ void ItemUseCB_RareCandy(u8 taskId, TaskFunc task)
     u8 holdEffectParam = ItemId_GetHoldEffectParam(*itemPtr);
 
     sInitialLevel = GetMonData(mon, MON_DATA_LEVEL);
-    if (!(B_RARE_CANDY_CAP && sInitialLevel >= GetCurrentLevelCap()))
+    if (!(B_RARE_CANDY_CAP && sInitialLevel >= (GetCurrentLevelCap()-1)))
     {
         BufferMonStatsToTaskData(mon, arrayPtr);
         cannotUseEffect = ExecuteTableBasedItemEffect(mon, *itemPtr, gPartyMenu.slotId, 0);
