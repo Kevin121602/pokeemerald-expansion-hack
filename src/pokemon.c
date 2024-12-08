e60dc4479ae67c59d48fefd439f3a8d20dd87e64
Kevin121602 <kevin.james.gracey@gmail.com>
2024-12-07 21:56:04 -0500

    more scripting updates

--- src/pokemon.c
+++ src/pokemon.c
@@ -5740,7 +5740,7 @@ u16 GetBattleBGM(void)
         case TRAINER_CLASS_TEAM_MAGMA:
         case TRAINER_CLASS_AQUA_ADMIN:
         case TRAINER_CLASS_MAGMA_ADMIN:
-            return MUS_VS_AQUA_MAGMA;
+            return MUS_DP_VS_GALACTIC;
         case TRAINER_CLASS_LEADER:
             return MUS_DP_VS_GYM_LEADER;
         case TRAINER_CLASS_CHAMPION:
