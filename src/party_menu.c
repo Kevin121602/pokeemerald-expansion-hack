439ffca5448cf10265f86741ecfa30169809a178
Kevin121602 <kevin.james.gracey@gmail.com>
2025-05-06 18:11:28 -0400

    removed field moves from the party menu

--- src/party_menu.c
+++ src/party_menu.c
@@ -2843,13 +2843,10 @@ static void SetPartyMonFieldSelectionActions(struct Pokemon *mons, u8 slotId)
     // Add field moves to action list
     for (i = 0; i < MAX_MON_MOVES; i++)
     {
-        for (j = 0; j != FIELD_MOVES_COUNT; j++)
+        if (GetMonData(&mons[slotId], i + MON_DATA_MOVE1) == MOVE_FLY)
         {
-            if (GetMonData(&mons[slotId], i + MON_DATA_MOVE1) == sFieldMoves[j])
-            {
-                AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, j + MENU_FIELD_MOVES);
-                break;
-            }
+            AppendToList(sPartyMenuInternal->actions, &sPartyMenuInternal->numActions, 5 + MENU_FIELD_MOVES);
+            break;
         }
     }
 
