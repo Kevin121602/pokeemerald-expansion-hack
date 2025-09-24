a5e4e3ef64a917cb6c28fabeffd399bc8355737d
Kevin121602 <kevin.james.gracey@gmail.com>
2025-09-23 22:14:33 -0400

    made trick house puzzles open with a script rather than automatically with gym badges

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1388,13 +1388,13 @@
 #define FLAG_IS_CHAMPION                            (SYSTEM_FLAGS + 0x1F) // Seems to be related to linking.
 #define FLAG_NURSE_UNION_ROOM_REMINDER              (SYSTEM_FLAGS + 0x20)
 
-#define FLAG_UNUSED_0x881                           (SYSTEM_FLAGS + 0x21) // Unused Flag
-#define FLAG_UNUSED_0x882                           (SYSTEM_FLAGS + 0x22) // Unused Flag
-#define FLAG_UNUSED_0x883                           (SYSTEM_FLAGS + 0x23) // Unused Flag
-#define FLAG_UNUSED_0x884                           (SYSTEM_FLAGS + 0x24) // Unused Flag
-#define FLAG_UNUSED_0x885                           (SYSTEM_FLAGS + 0x25) // Unused Flag
-#define FLAG_UNUSED_0x886                           (SYSTEM_FLAGS + 0x26) // Unused Flag
-#define FLAG_UNUSED_0x887                           (SYSTEM_FLAGS + 0x27) // Unused Flag
+#define FLAG_HEAT_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x21) // Unused Flag
+#define FLAG_KNUCKLE_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x22) // Unused Flag
+#define FLAG_BALANCE_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x23) // Unused Flag
+#define FLAG_RAIN_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x24) // Unused Flag
+#define FLAG_FEATHER_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x25) // Unused Flag
+#define FLAG_MIND_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x26) // Unused Flag
+#define FLAG_DYNAMO_BADGE_TRICK_HOUSE_OPEN                           (SYSTEM_FLAGS + 0x27) // Unused Flag
 
 #define FLAG_SYS_USE_FLASH                          (SYSTEM_FLAGS + 0x28)
 #define FLAG_SYS_USE_STRENGTH                       (SYSTEM_FLAGS + 0x29)
